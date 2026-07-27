94586fe4154926ab72d0d4c6acc07a83efa1cb5c
Cleber Severo <clebersevero1998@gmail.com>
2026-07-26 22:58:56 -0300

    feat: add get controller and config book entity

---
+++ Controllers/BookController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() {
+        return Ok("Livros");
+    }
+}
---
+++ Entity/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryManagementApi.Entity;
+
+public class Book
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+}
